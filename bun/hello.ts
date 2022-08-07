5e48191cd28f4ab927723acc367a86a6d20ef43d
Yusuke Wada <yusuke@kamawada.com>
2022-08-08 00:32:00 +0900

    update Bun example. more practical

--- bun/hello.ts
+++ bun/hello.ts
@@ -6,6 +6,9 @@ app.get('/', (c) => {
   return c.json({ message: 'Hello Bun!' })
 })
 
+const port = parseInt(process.env.PORT) || 3000
+console.log(`Running at http://localhost:${port}`)
+
 export default {
   port: 3000,
   fetch: app.fetch,
--- bun/jsx.tsx
+++ bun/jsx.tsx
@@ -1,4 +1,5 @@
 import { Hono } from 'hono'
+import { serveStatic } from 'hono/serve-static.bun'
 
 const app = new Hono()
 
@@ -23,9 +24,14 @@ const Top = (props: { messages: string[] }) => {
   )
 }
 
+app.use('/favicon.ico', serveStatic({ path: './public/favicon.ico' }))
+
 app.get('/', (c) => {
   const messages = ['Good Morning', 'Good Evening', 'Good Night']
   return c.html(<Top messages={messages} />)
 })
+
+const port = parseInt(process.env.PORT) || 3000
+console.log(`Running at http://localhost:${port}`)
 
 export default app
