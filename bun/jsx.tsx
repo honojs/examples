22766813507b401c76775a0a4423e478e58819a9
Mountain/\Ash <mountainash@rmwpublishing.net>
2023-03-27 00:40:27 +0200

    Update to bun example to make use of the port (that's in the env)

--- bun/jsx.tsx
+++ bun/jsx.tsx
@@ -35,4 +35,7 @@ app.get('/', (c) => {
 const port = parseInt(process.env.PORT!) || 3000
 console.log(`Running at http://localhost:${port}`)
 
-export default app
+export default {
+  port,
+  fetch: app.fetch
+};
