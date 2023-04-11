a25c6f939dfd6856291daa224bbddaa2e6834179
Yusuke Wada <yusuke@kamawada.com>
2023-04-11 23:42:17 +0900

    bump up Hono v3.1.5

--- deno/hello.ts
+++ deno/hello.ts
@@ -1,6 +1,6 @@
 import { serve } from 'https://deno.land/std@0.146.0/http/server.ts'
-import { Hono } from 'https://deno.land/x/hono@v3.0.2/mod.ts'
-import { logger, poweredBy } from 'https://deno.land/x/hono@v3.0.2/middleware.ts'
+import { logger, poweredBy } from 'https://deno.land/x/hono@v3.1.5/middleware.ts'
+import { Hono } from 'https://deno.land/x/hono@v3.1.5/mod.ts'
 
 const app = new Hono()
 
