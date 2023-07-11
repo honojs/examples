6d4897e6ae9d0505b2a326776eb60bcb3aba67a1
Yusuke Wada <yusuke@cloudflare.com>
2023-07-11 19:02:44 +0900

    bump up Hono and use `Deno.serve()`

--- deno/hello.ts
+++ deno/hello.ts
@@ -1,6 +1,5 @@
-import { serve } from 'https://deno.land/std@0.146.0/http/server.ts'
-import { logger, poweredBy } from 'https://deno.land/x/hono@v3.1.5/middleware.ts'
-import { Hono } from 'https://deno.land/x/hono@v3.1.5/mod.ts'
+import { logger, poweredBy } from 'https://deno.land/x/hono@v3.3.0/middleware.ts'
+import { Hono } from 'https://deno.land/x/hono@v3.3.0/mod.ts'
 
 const app = new Hono()
 
@@ -9,4 +8,4 @@ app.get('/', (c) => {
   return c.text('Hello Deno!')
 })
 
-serve(app.fetch)
+Deno.serve(app.fetch)
