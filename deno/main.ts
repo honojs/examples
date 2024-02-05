0e1eca9eaf22d2fcb0f3a67b0a3b6523845dc5de
Andy Jiang <andyjiang@gmail.com>
2024-02-05 12:34:50 -0800

    deno fmt

--- deno/main.ts
+++ deno/main.ts
@@ -1,11 +1,14 @@
-import { logger, poweredBy } from 'https://deno.land/x/hono@v3.11.11/middleware.ts'
-import { Hono } from 'https://deno.land/x/hono@v3.11.11/mod.ts'
+import {
+  logger,
+  poweredBy,
+} from "https://deno.land/x/hono@v3.11.11/middleware.ts";
+import { Hono } from "https://deno.land/x/hono@v3.11.11/mod.ts";
 
-const app = new Hono()
+const app = new Hono();
 
-app.use('*', logger(), poweredBy())
-app.get('/', (c) => {
-  return c.text('Hello Deno!')
-})
+app.use("*", logger(), poweredBy());
+app.get("/", (c) => {
+  return c.text("Hello Deno!");
+});
 
-Deno.serve(app.fetch)
+Deno.serve(app.fetch);
