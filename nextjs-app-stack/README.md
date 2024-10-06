87c597c7f39a7d26569fc1ce421ff815abfb3082
Steven Sermeus <stevensermeus@gmail.com>
2024-10-06 07:28:37 +0200

    Add an example for Next.js App router
    Example with Next.js app router, zod validation middleware and
    react-query.

---
+++ nextjs-app-stack/README.md
@@ -0,0 +1,23 @@
+# Hono stack with Next.js App router
+
+- Next.js
+- Hono
+- Zod
+- Zod Validator Middleware
+- `hc`
+- React query
+
+## Usage
+
+```
+yarn
+yarn dev
+```
+
+## Author
+
+Sermeus Steven <https://github.com/StevenSermeus>
+
+## License
+
+MIT
---
+++ nextjs-app-stack/app/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { $api } from "@/lib/api";
+import { useMutation } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+import { useRef } from "react";
+
+export default function Home() {
+  const $post = $api.echo.$post;
+  const nameRef = useRef<HTMLInputElement>(null);
+  const mutation = useMutation<
+    InferResponseType<typeof $post>,
+    Error,
+    InferRequestType<typeof $post>["json"]
+  >({
+    mutationFn: async (data) => {
+      const res = await $post({
+        json: data,
+      });
+      return await res.json();
+    },
+    mutationKey: ["echo"],
+  });
+
+  return (
+    <>
+      <input ref={nameRef} type="text" placeholder="Name" />
+      <button
+        onClick={() => {
+          mutation.mutate({
+            name: nameRef.current?.value || "",
+          });
+        }}
+      >
+        Send
+      </button>
+      {mutation.isSuccess && <div>Hello {mutation.data.name}</div>}
+    </>
+  );
+}
