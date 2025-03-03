7e5095d8f3b273110f22b0a066c720828409df7a
Erik Beuschau <bager.erik@gmail.com>
2025-03-03 15:15:08 +0100

    Remove irrelevant test file

--- serve-static/src/index.test.ts
+++
@@ -1,9 +0,0 @@
-import app from './index'
-
-describe('Example', () => {
-  test('GET /', async () => {
-    const res = await app.request('http://localhost/')
-    expect(res.status).toBe(200)
-    expect(res.headers.get('x-powered-by')).toBe('Hono')
-  })
-})
